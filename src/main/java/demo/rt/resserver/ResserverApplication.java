ad7fac1c4d00d7a2783b9114e36da541167280e0
cap <civil.aviation.pro@gmail.com>
2025-05-10 13:42:56 -0600

    first commit

---
+++ src/main/java/demo/rt/resserver/ResserverApplication.java
@@ -0,0 +1,13 @@
+package demo.rt.resserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demo/rt/resserver/ResserverApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.rt.resserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
